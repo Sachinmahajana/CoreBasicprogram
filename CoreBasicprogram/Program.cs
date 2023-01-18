56333ce619957a2b16d7e07ccf7435a739b66595
Sachinmahajana <sachinmahajan782@gmail.com>
2023-01-19 00:08:04 +0530

    [Sachin] Adding the Basic core program

---
+++ CoreBasicprogram/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace CoreBasicprogram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Basi core program");
+            Console.ReadLine();
+        }
+    }
+}
