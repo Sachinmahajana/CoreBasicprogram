9009a29b51d3b66f9c010f5a4abc643018aac343
Sachinmahajana <sachinmahajan782@gmail.com>
2023-01-28 18:23:34 +0530

    [Sachin] Adding LeapYear

---
+++ CoreBasicprogram/LeapYear.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreBasicprogram
+{
+    internal class LeapYear
+    {
+        public void LeapYearFun()
+        {
+            Console.WriteLine("Enter the Year -");
+            // Taking user input.
+            int year = Convert.ToInt32(Console.ReadLine());
+            // Checking the condition to finding leap year.
+            if(year % 400 == 0) 
+            {
+                Console.WriteLine("{0} is a leap year", year);
+            }
+            else if(year % 100 == 0)
+            {
+                Console.WriteLine("{0} is not a leap year", year);
+            }
+            else if(year % 4 == 0)
+            {
+                Console.WriteLine("{0} is a leap year", year);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a leap year", year);
+            }
+        }
+        
+    }
+}
--- CoreBasicprogram/Program.cs
+++ CoreBasicprogram/Program.cs
@@ -5,18 +5,37 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Basic core program");
-            //EvenOddFun myObj = new EvenOddFun();
-            //myObj.EvenOdd();
-            //Evenodd1 myObj1 = new Evenodd1();
-            //myObj1.Even();
+            Console.WriteLine("\n1.EvenOddFun \n2.Largenumber \n3.Swapnumber \n4.LeapYear \n5.EvenOdd1");
+            int option = Convert.ToInt32(Console.ReadLine());
 
-            //Large myObj2 = new Large();
-            //myObj2.LargeFun();
-
-            Swapnumber myObj3 = new Swapnumber();
-            myObj3.SwapNumber();
-
+            switch (option)
+            {
+                case 1:
+                    EvenOddFun myObj = new EvenOddFun();
+                    myObj.EvenOdd();
+                    break;
+                case 2:
+                    Large myObj2 = new Large();
+                    myObj2.LargeFun();
+                    break;
+                case 3:
+                    Swapnumber myObj3 = new Swapnumber();
+                    myObj3.SwapNumber();
+                    break;
+                case 4:
+                    LeapYear myObj4 = new LeapYear();
+                    myObj4.LeapYearFun();
+                    break;
+                case 5:
+                    Evenodd1 myObj1 = new Evenodd1();
+                    myObj1.Even();
+                    break;
+                default:
+                    Console.WriteLine("Choose from given option only!");
+                    break;
+            }
             Console.ReadLine();
+           
         }
     }
 }
